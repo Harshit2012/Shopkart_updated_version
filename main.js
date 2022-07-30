b00f9e1f50ad06319a6cf37c7859112f4503f6c3
Harshit2012 <105143145+Harshit2012@users.noreply.github.com>
2022-07-30 21:22:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function updated_version(){
+    window.location="index2.html";
+}
+
+function back(){
+    window.location="index.html"
+}
+
+function myntra(){
+    window.location="https://www.myntra.com/"
+}
